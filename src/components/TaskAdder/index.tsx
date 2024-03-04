54ffa313a971ddf5cf7d911f53c97b71f1aab02d
April <95392008+apriltaoyvr@users.noreply.github.com>
2024-03-04 00:32:49 -0800

    Add tooltips; additional comments

--- src/components/TaskAdder/index.tsx
+++ src/components/TaskAdder/index.tsx
@@ -1,11 +1,26 @@
+'use client'
+// UI
 import { Input, Button } from '@/components/ui';
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip"
 import { PlusIcon } from '@radix-ui/react-icons';
+// Hooks
 import { useAppDispatch, useAppSelector } from '@/lib/redux/hooks';
+// Reducers
 import { editName } from '@/lib/redux/slices/task';
 import { addTask } from '@/lib/redux/slices/taskList';
+// Types
 import type { RootState } from '@/lib/redux/store';
 import type { Task } from '@/types/task';
 
+// NOTE: The reducers from task and taskList serve different purposes
+// task is used to edit the task name
+// taskList is used to add the task to the list
+// We compartmentalize the logic for future-proofing
 export default function TaskAdder({ tasks }: { tasks: Task[] }) {
   const dispatch = useAppDispatch();
   const task = useAppSelector((state: RootState) => state.task);
@@ -19,15 +34,24 @@ export default function TaskAdder({ tasks }: { tasks: Task[] }) {
         onChange={(e) => dispatch(editName(e.target.value))}
         onKeyDown={(e) => e.key === 'Enter' && dispatch(addTask(task))}
       />
-      <Button
-        id='submit'
-        aria-label='Submit task'
-        variant='outline'
-        onClick={() => dispatch(addTask(task))}
-        disabled={!task.name}
-      >
-        <PlusIcon />
-      </Button>
+      <TooltipProvider>
+        <Tooltip>
+          <TooltipTrigger>
+            <Button
+              id='submit'
+              aria-label='Submit task'
+              variant='outline'
+              onClick={() => dispatch(addTask(task))}
+              disabled={!task.name}
+            >
+              <PlusIcon />
+            </Button>
+          </TooltipTrigger>
+          <TooltipContent>
+            <p>Add to todo list</p>
+          </TooltipContent>
+        </Tooltip>
+      </TooltipProvider>
     </>
   );
 }
--- src/components/TaskItem/index.tsx
+++ src/components/TaskItem/index.tsx
@@ -1,5 +1,5 @@
 'use client';
-import { Button } from '@/components/ui';
+// UI
 import { cn } from '@/lib/utils';
 import {
   TrashIcon,
@@ -8,12 +8,22 @@ import {
   StarIcon,
   StarFilledIcon,
 } from '@radix-ui/react-icons';
+import { Button } from '@/components/ui';
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip"
+// Hooks
 import { useAppDispatch, useAppSelector } from '@/lib/redux/hooks';
+// Reducers
 import {
   removeTask,
   toggleStarred,
   toggleCompletion,
 } from '@/lib/redux/slices/taskList';
+// Types
 import type { Task as TaskType } from '@/types/task';
 
 export default function TaskItem({ task }: { task: TaskType }) {
@@ -31,20 +41,38 @@ export default function TaskItem({ task }: { task: TaskType }) {
         {task.name}
       </span>
       <div>
-        <Button
-          aria-label='Star task'
-          variant='ghost'
-          onClick={() => dispatch(toggleStarred(task))}
-        >
-          {task.starred ? <StarFilledIcon /> : <StarIcon />}
-        </Button>
-        <Button
-          aria-label='Mark task as complete'
-          variant='ghost'
-          onClick={() => dispatch(toggleCompletion(task))}
-        >
-          {task.complete ? <Cross1Icon /> : <CheckIcon />}
-        </Button>
+        <TooltipProvider>
+          <Tooltip>
+            <TooltipTrigger>
+              <Button
+                aria-label='Star task'
+                variant='ghost'
+                onClick={() => dispatch(toggleStarred(task))}
+              >
+                {task.starred ? <StarFilledIcon /> : <StarIcon />}
+              </Button>
+            </TooltipTrigger>
+            <TooltipContent>
+              <p>Star task</p>
+            </TooltipContent>
+          </Tooltip>
+        </TooltipProvider>
+        <TooltipProvider>
+          <Tooltip>
+            <TooltipTrigger>
+              <Button
+                aria-label='Mark task as complete'
+                variant='ghost'
+                onClick={() => dispatch(toggleCompletion(task))}
+              >
+                {task.complete ? <Cross1Icon /> : <CheckIcon />}
+              </Button>
+            </TooltipTrigger>
+            <TooltipContent>
+              <p>Mark as complete</p>
+            </TooltipContent>
+          </Tooltip>
+        </TooltipProvider>
       </div>
       <Button
         aria-label='Delete task'
@@ -52,7 +80,7 @@ export default function TaskItem({ task }: { task: TaskType }) {
         className={'group'}
         onClick={() => dispatch(removeTask(task))}
       >
-        <TrashIcon className='transition-colors group-hover:text-red-600 ' />
+        <TrashIcon className='transition-colors group-hover:text-red-600' />
       </Button>
     </li>
   );
